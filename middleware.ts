7986a0027cf62572c72e968fa3beca78ee377299
Skybitsky <dskybitsky@gmail.com>
2024-09-22 09:03:26 -0400

    HOTFIX Db Settings

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const [AUTH_USER, AUTH_PASS] = (process.env.HTTP_BASIC_AUTH || ':').split(':');
+const [APP_AUTH_USER, APP_AUTH_PASS] = (process.env.APP_AUTH || ':').split(':');
 
 export function middleware(req: NextRequest) {
-    if (!isAuthenticated(req)) {
+    if (APP_AUTH_USER && APP_AUTH_PASS && !isAuthenticated(req)) {
         return new NextResponse('Authentication required', {
             status: 401,
             headers: { 'WWW-Authenticate': 'Basic' },
@@ -25,7 +25,7 @@ function isAuthenticated(req: NextRequest) {
     const user = auth[0];
     const pass = auth[1];
 
-    return user == AUTH_USER && pass == AUTH_PASS;
+    return user == APP_AUTH_USER && pass == APP_AUTH_PASS;
 }
 
 export const config = {
